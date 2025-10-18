e97f96b59af8fbe250e6ae3b340b808b231b2132
Luis Felipe Castro Calderón <96279606+wherelucho95@users.noreply.github.com>
2025-10-18 10:47:44 -0500

    Committing 1 item from workspace bf39c7eb-674e-42a1-a9d4-cc490c8fbec0

--- StagingWarehouseForDataflows_20251018153244.Warehouse/dbo/Tables/Salary_Data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Salary_Data] (
-
-	[Age] varchar(8000) NULL, 
-	[Gender] varchar(8000) NULL, 
-	[Education Level] varchar(8000) NULL, 
-	[Job Title] varchar(8000) NULL, 
-	[Years of Experience] varchar(8000) NULL, 
-	[Salary] varchar(8000) NULL
-);
